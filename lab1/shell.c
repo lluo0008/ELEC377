29c3078e670c795b590e3475523407d6210bc728
Lawrence Luo <xllluo08@gmail.com>
2020-09-27 22:48:02 -0700

    more testing

--- lab1/shell.c
+++ lab1/shell.c
@@ -380,7 +380,7 @@ void pwdFunc(char *args[], int nargs)
 
 int dotCheck(const struct dirent *d)
 {
-	printf("TEST TEST TEST");
+	exit(0);
 	//this checks if the first file in the directory contains a '.'
 	if (d->d_name[0] == '.'){
 		return 0;
