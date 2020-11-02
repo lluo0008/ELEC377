0ed96c41f79addf3acee3df247180813d8073927
Lawrence Luo <xllluo08@gmail.com>
2020-11-01 19:40:15 -0800

    changed producer code

--- lab3/producer.c
+++ lab3/producer.c
@@ -50,27 +50,35 @@ int main (int argc, char *argv[]){
 	getMutex(&sharedPtr -> lock);
 	sharedPtr -> numProducers++;
 	releaseMutex(&sharedPtr -> lock);
-	while(getchar() != EOF)
+	while((c = getchar()) != EOF)
 	{
 		stored = FALSE;
 		while(stored == FALSE)
 		{
 			getMutex(&sharedPtr -> lock);
-			if (sizeof(sharedPtr -> buffer) != BUFFSIZE)
 
-			int counter;
-			int i;
-			for (i = 0; i < BUFFSIZE; i++) //checking the amount of elements in the buffer array
+			// int counter;
+			// int i;
+			// for (i = 0; i < BUFFSIZE; i++) //checking the amount of elements in the buffer array
+			// {
+			// 	if (sharedPtr -> buffer[i] != NULL || sharedPtr -> buffer[i] != '\0')
+			// 	{
+			// 		counter++;
+			// 	}
+			// }
+			// if (counter != BUFFSIZE) //checking if the buffer array still has room left in it (i.e. it is not full)
+			// {
+			// 	stored = TRUE;
+			// }
+
+			if(sharedPtr -> count < BUFFSIZE)
 			{
-				if (sharedPtr -> buffer[i] != NULL || sharedPtr -> buffer[i] != '\0')
-				{
-					counter++;
-				}
-			}
-			if (counter != BUFFSIZE) //checking if the buffer array still has room left in it (i.e. it is not full)
-			{
+				sharedPtr -> buffer[sharedPtr -> in] = c;
+				sharedPtr -> in = (sharedPtr -> in + 1) % BUFFSIZE;
+				sharedPtr -> count++;
 				stored = TRUE;
 			}
+			
 			releaseMutex(&sharedPtr -> lock);
 		}
 	}
