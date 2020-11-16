51f4d760ecfe92bd495ca0513fa04b3c838f262f
Lawrence Luo <xllluo08@gmail.com>
2020-11-15 22:58:45 -0800

    added find for .c and .h

--- lab4/printDoc.sh
+++ lab4/printDoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "============================================================="
 
-for file in $(find .); do
+for file in $(find . -type f \( -name '*.c' -o -name '*.h' \)); do
     sed -e '/\/\/+/,/\/\/-/p' -e 'd' $file | sed -e 's/\/\///;s/-//;s/+//'
     #sed -e '/\/\/+/,/\/\/-/p' -e 'd' $file | sed -e 's/\/\/-//'
     #sed -e '/\/\/+/,/\/\/-/p' -e 'd' $file | sed -e 's/\/\/+//'
