082f54208570f7d2452d1f040f173f53881f2ce8
Lawrence Luo <xllluo08@gmail.com>
2020-11-15 22:17:28 -0800

    asvwe

--- lab4/progStats.sh
+++ lab4/progStats.sh
@@ -12,8 +12,8 @@ modCheck=false
 if [ -d "$1" ]; then
     echo "Main Files: "
     for file in 'find $1 -type f -name "*.c"'; do
-        grep "int main" $file -e
-        echo test
+        grep "int main" $file -q
+        echo $file
         if (( $? == 0 )); then
             
             printfNum='grep -c "[^f]printf" $file'
