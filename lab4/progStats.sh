62031dfceffc2f8a7dded9d324a75cff0b5ca6a7
Lawrence Luo <xllluo08@gmail.com>
2020-11-15 21:32:28 -0800

    changed some quotes

--- lab4/progStats.sh
+++ lab4/progStats.sh
@@ -14,8 +14,8 @@ if [ -d "$1" ]; then
     for file in 'find "$1" -type f -name '*.c''; do
         if grep -q "int main" $file
             then
-                printfNum="'grep -c printf $file'"
-                fprintfNum="'grep -c fprintf $file'"
+                printfNum='grep -c "printf" $file'
+                fprintfNum='grep -c "fprintf" $file'
                 mainCheck=true
                 echo "$file $printfNum $fprintfNum" | awk '{printf "%s: %d,%d\n", $1, $2, $3}'
         fi
