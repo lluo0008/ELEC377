8bff886a620c74f81c07f2a094dadffacfdc9ffc
Lawrence Luo <xllluo08@gmail.com>
2020-12-06 21:50:19 -0500

    98x

--- lab5/selfcomp.c
+++ lab5/selfcomp.c
@@ -10,7 +10,7 @@ void foo();
 int main(){
 
     char *exploit =
-     "XXXXXXXXXXXXXXXX"
+     "XXXXXXXXXXXXXXXXXX"
      "XXXXXXXXXXXXXXXXXXXX"
      "XXXXXXXXXXXXXXXXXXXX"
 	 "XXXXXXXXXXXXXXXXXXXX"
